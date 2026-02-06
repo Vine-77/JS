74f3a81f57698fd359d3c10e7693a8267514a9fe
Vine 77 <shemadivin7@gmail.com>
2026-02-06 21:14:29 +0200

    Created an online ordering platform

---
+++ promises.js
@@ -0,0 +1,11 @@
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 200], ['basketballs', 30]];
+
+checkInventory(order)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
