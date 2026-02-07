5fc5c799f501b6040064140ee9d7cfe9789a0588
Vine 77 <shemadivin7@gmail.com>
2026-02-07 20:30:59 +0200

    made a cleaner version

--- library.js
+++ library.js
@@ -9,7 +9,7 @@ const checkInventory = (order) =>
             {
                 let inStock = order.every(item => inventory[item[0]] >= item[1]);
                 if (inStock) {
-                    resolve('Your order has been successfully pressed! Thanks for using our services!');
+                    resolve('Your order has been successfully pressed! And thanks for using our services!');
                 }
                 else {
                     reject('We are sorry. Your order could not be completed because some items are sold out or exceed the stock!');
--- promises.js
+++ promises.js
@@ -8,4 +8,4 @@ checkInventory(order)
     })
     .catch((error) => {
         console.log(error);
-    });
+    })
