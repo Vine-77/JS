87367d31060fc7ebc7e50fdd0b62fa7a58153191
Vine-77 <shemadivin7@gmail.com>
2026-03-29 22:34:48 +0200

    Adds new changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let count = 0;
 let counter = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 function incrementer() {
   count += 1;
   counter.innerText = count
 }
 function reset() {
     count = 0
-    console.log("clicked!")
-    counter.innerText = count
+    counter.textContent = count
 }
 
 function save() {
-    console.log(count)
+    let history = count + " - "
+    saveEl.textContent += history
+    reset()
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+let welcomeEl = document.getElementById("welcome-el")
+let name = "Divin"
+let greeting = "What's up! "
+
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText += "👋🏼"
